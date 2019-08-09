87421c059a8d107c8b1234ce305bd34cea385c06
Aluno <renanandreolla@outlook.com>
2019-08-08 21:26:19 -0300

    Soma de numeros pares de 2 a aleatório

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 //3) Ler um número inteiro n. Escrever a soma de todos os números
-//pares de 2 até n
+//pares de 2 até n.
+
+var num = parseInt(prompt("Digite um número aqui:"));
+var soma = 2;
+
+while (soma < num) {
+
+  if(num%2 == 0) {
+    soma = num + 2
+    soma++
+    document.write(soma)
+  }
+}
